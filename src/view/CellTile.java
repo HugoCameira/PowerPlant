8d63352a233b4949e69b937b92ef959a5e49c13b
HugoCameira <hugo.cameira@hotmail.com>
2018-04-04 14:05:55 +0100

    View bugfixing

--- src/view/CellTile.java
+++ src/view/CellTile.java
@@ -20,48 +20,48 @@ public abstract class CellTile extends Tile {
     @Override
     public void paint() {
         if (cell.mDirecions[0]) {
-            // backGroundChange(cell);
+            backGroundChange(cell);
             //Console.cursor(line * CELL_HEIGHT + cell.positionUp.line, col * CELL_WIDTH + cell.positionUp.col);
             print(0, SIDE / 2, '|');
 
             //Console.print('|');
         } else {
             //Console.cursor(line * CELL_HEIGHT + cell.positionUp.line, col * CELL_WIDTH + cell.positionUp.col);
-            //Console.setBackground(Console.BLACK);
+            Console.setBackground(Console.BLACK);
             print(0, SIDE / 2, ' ');
             //Console.print(' ');
         }
         if (cell.mDirecions[1]) {
             //Console.cursor(line * CELL_HEIGHT + cell.positionRight.line, col * CELL_WIDTH + cell.positionRight.col);
             backGroundChange(cell);
-            print(SIDE / 2, SIDE, '-');
+            print(SIDE / 2, SIDE-1, '-');
             //Console.print('-');
         } else {
             // Console.cursor(line * CELL_HEIGHT + cell.positionRight.line, col * CELL_WIDTH + cell.positionRight.col);
             Console.setBackground(Console.BLACK);
-            print(SIDE / 2, SIDE, ' ');
+            print(SIDE / 2, SIDE-1, ' ');
             // Console.print(' ');
         }
         if (cell.mDirecions[2]) {
             // Console.cursor(line * CELL_HEIGHT + cell.positionDown.line, col * CELL_WIDTH + cell.positionDown.col);
             backGroundChange(cell);
-            print(SIDE, SIDE / 2, '|');
+            print(SIDE-1, SIDE / 2, '|');
             // Console.print('|');
         } else {
             //Console.cursor(line * CELL_HEIGHT + cell.positionDown.line, col * CELL_WIDTH + cell.positionDown.col);
             Console.setBackground(Console.BLACK);
-            print(SIDE, SIDE / 2, ' ');
+            print(SIDE-1, SIDE / 2, ' ');
             //Console.print(' ');
         }
         if (cell.mDirecions[3]) {
             // Console.cursor(line * CELL_HEIGHT + cell.positionLeft.line, col * CELL_WIDTH + cell.positionLeft.col);
             backGroundChange(cell);
-            print(0, SIDE / 2, '-');
+            print(SIDE / 2, 0, '-');
             //  Console.print('-  ');
         } else {
             // Console.cursor(line * CELL_HEIGHT + cell.positionLeft.line, col * CELL_WIDTH + cell.positionLeft.col);
             Console.setBackground(Console.BLACK);
-            print(0, SIDE / 2, ' ');
+            print(SIDE / 2, 0, ' ');
             //  Console.print(' ');
         }
         backGroundChangeCenter(cell);
@@ -80,7 +80,7 @@ public abstract class CellTile extends Tile {
         Cell[][] newBoard = model.getBoard();
         for (int line = 0; line < model.getHeight(); line++) {
             for (int col = 0; col < model.getWidth(); col++) {
-                drawCell(newBoard[line][col], line, col);
+                // drawCell(newBoard[line][col], line, col);
             }
         }
     }
@@ -116,50 +116,51 @@ public abstract class CellTile extends Tile {
 
     }
 
-    private void drawCell(Cell piece, int line, int col) {
+    /*
+        private void drawCell(Cell piece, int line, int col) {
 
-        if (piece.mDirecions[0]) {
-            Console.cursor(line * CELL_HEIGHT + piece.positionUp.line, col * CELL_WIDTH + piece.positionUp.col);
-            backGroundChange(piece);
-            Console.print('|');
-        } else {
-            Console.cursor(line * CELL_HEIGHT + piece.positionUp.line, col * CELL_WIDTH + piece.positionUp.col);
-            Console.setBackground(Console.BLACK);
-            Console.print(' ');
-        }
-        if (piece.mDirecions[1]) {
-            Console.cursor(line * CELL_HEIGHT + piece.positionRight.line, col * CELL_WIDTH + piece.positionRight.col);
-            backGroundChange(piece);
-            Console.print('-');
-        } else {
-            Console.cursor(line * CELL_HEIGHT + piece.positionRight.line, col * CELL_WIDTH + piece.positionRight.col);
-            Console.setBackground(Console.BLACK);
-            Console.print(' ');
-        }
-        if (piece.mDirecions[2]) {
-            Console.cursor(line * CELL_HEIGHT + piece.positionDown.line, col * CELL_WIDTH + piece.positionDown.col);
-            backGroundChange(piece);
-            Console.print('|');
-        } else {
-            Console.cursor(line * CELL_HEIGHT + piece.positionDown.line, col * CELL_WIDTH + piece.positionDown.col);
-            Console.setBackground(Console.BLACK);
-            Console.print(' ');
-        }
-        if (piece.mDirecions[3]) {
-            Console.cursor(line * CELL_HEIGHT + piece.positionLeft.line, col * CELL_WIDTH + piece.positionLeft.col);
-            backGroundChange(piece);
-            Console.print('-');
-        } else {
-            Console.cursor(line * CELL_HEIGHT + piece.positionLeft.line, col * CELL_WIDTH + piece.positionLeft.col);
-            Console.setBackground(Console.BLACK);
-            Console.print(' ');
-        }
-        backGroundChangeCenter(piece);
-        Console.cursor(line * CELL_HEIGHT + piece.positionCenter.line, col * CELL_WIDTH + piece.positionCenter.col);
-        Console.print(piece.mCenter);
+            if (piece.mDirecions[0]) {
+                Console.cursor(line * CELL_HEIGHT + piece.positionUp.line, col * CELL_WIDTH + piece.positionUp.col);
+                backGroundChange(piece);
+                Console.print('|');
+            } else {
+                Console.cursor(line * CELL_HEIGHT + piece.positionUp.line, col * CELL_WIDTH + piece.positionUp.col);
+                Console.setBackground(Console.BLACK);
+                Console.print(' ');
+            }
+            if (piece.mDirecions[1]) {
+                Console.cursor(line * CELL_HEIGHT + piece.positionRight.line, col * CELL_WIDTH + piece.positionRight.col);
+                backGroundChange(piece);
+                Console.print('-');
+            } else {
+                Console.cursor(line * CELL_HEIGHT + piece.positionRight.line, col * CELL_WIDTH + piece.positionRight.col);
+                Console.setBackground(Console.BLACK);
+                Console.print(' ');
+            }
+            if (piece.mDirecions[2]) {
+                Console.cursor(line * CELL_HEIGHT + piece.positionDown.line, col * CELL_WIDTH + piece.positionDown.col);
+                backGroundChange(piece);
+                Console.print('|');
+            } else {
+                Console.cursor(line * CELL_HEIGHT + piece.positionDown.line, col * CELL_WIDTH + piece.positionDown.col);
+                Console.setBackground(Console.BLACK);
+                Console.print(' ');
+            }
+            if (piece.mDirecions[3]) {
+                Console.cursor(line * CELL_HEIGHT + piece.positionLeft.line, col * CELL_WIDTH + piece.positionLeft.col);
+                backGroundChange(piece);
+                Console.print('-');
+            } else {
+                Console.cursor(line * CELL_HEIGHT + piece.positionLeft.line, col * CELL_WIDTH + piece.positionLeft.col);
+                Console.setBackground(Console.BLACK);
+                Console.print(' ');
+            }
+            backGroundChangeCenter(piece);
+            Console.cursor(line * CELL_HEIGHT + piece.positionCenter.line, col * CELL_WIDTH + piece.positionCenter.col);
+            Console.print(piece.mCenter);
 
-    }
-
+        }
+    */
     public static Tile newInstance(Cell cell) {
         switch (cell.getType()) {
             case '.':
